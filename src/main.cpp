1084b032e511b63bb540571dfebe0b0e96c9bcb5
Jaques Pereira <Jaques Pereira>
2017-03-22 21:31:10 -0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 // Include the libraries we need
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <SimpleTimer.h>
 #include <Atuadores.h>
+#include <SimpleTimer.h>
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 14
@@ -13,135 +13,99 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
-DIO luz(12);
-DIO heater(6);
-DIO sirene(5);
+DIO heater(12);
+DIO siren(6);
 SimpleTimer timer;
-
-float _gradient;
-unsigned int _stepTime;
-float _currentTemperature;
-float _desiredTemperature;
-float _initialTemperature;
-float _endTemperature;
-int _count;
-bool activeSirene = false;
-
-void iniciarBrassagem(unsigned int stepTime, float temperature);
-void callback_temperature();
-void updateDesiredTemperature ();
-void callback_sirene();
 /*
  * The setup function. We only start the sensors here
  */
-void setup()
+float desiredTemperature=0;
+float histerese = 1;
+int count = 0;
+int step = 0;
+float _stepTemperature = 0;
+float initialTemperature = 0;
+float currentTemperature ();
+void brassagem();
+void heaterTemperature(float desiredTemperature);
+float valueHisterese();
+void stepTemperature();
+void sireneOFF();
+
+bool modeBrassagem = false;
+bool modeFervura = false;
+bool activeHisterese = false;
+
+void setup(void)
 {
   // start serial port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
 
   // Start up the library
   sensors.begin();
-  timer.setInterval(5000, callback_temperature);
-
-  luz.on();
-  luz.print();
-  delay(3000);
-  luz.off();
-  luz.print();
-  delay(3000);
-  if (!luz.state()) {
-    luz.toggle();
-    delay(300);
-    luz.toggle();
-    delay(300);
-    luz.toggle();
-    delay(300);
-    luz.toggle();
-    delay(300);
-    luz.toggle();
-    delay(300);
-    luz.toggle();
-    delay(3000);
-  }
-  luz.toggle();
-  luz.print();
-  delay(3000);
-  luz.toggle();
-  luz.print();
-  delay(3000);
-  if (luz.state()) {
-    luz.toggle();
-    delay(300);
-    luz.toggle();
-    delay(300);
-    luz.toggle();
-    delay(300);
-  }
 }
 
 /*
  * Main function, get and show the temperature
  */
-void loop()
+void loop(void)
 {
-  timer.run();
+  //
+}
 
-  if (_currentTemperature < _desiredTemperature) {
+void brassagem()
+{
+  timer.setTimer(60000, stepTemperature, step);
+  heaterTemperature(desiredTemperature);
+}
+
+void iniciarStep () {
+  timer.setTimer(60000, stepTemperature, step);
+  count = 1;
+  _stepTemperature = (desiredTemperature - initialTemperature) / step;
+  desiredTemperature = desiredTemperature + (count * _stepTemperature);
+  heaterTemperature(desiredTemperature);
+
+}
+
+
+void stepTemperature ()
+{
+  count++;
+  desiredTemperature = desiredTemperature + (count * _stepTemperature);
+  if (step == count - 1) {
+    timer.setTimeout(3000, sireneOFF);
+    siren.on();
+  }
+}
+
+void sireneOFF () {
+  siren.off();
+}
+void heaterTemperature(float _desiredTemperature)
+{
+  if (currentTemperature() + valueHisterese() < _desiredTemperature) { //adicionar histerese
     heater.on();
+    activeHisterese = false;
   } else {
     heater.off();
-  }
-  if (activeSirene == true) {
-    timer.setTimer(2000, callback_sirene, 2);
-    activeSirene = false;
+    activeHisterese = true;
   }
 }
 
-void callback_sirene () {
-  sirene.toggle();
-}
-
-void iniciarBrassagem(unsigned int stepTime, float temperature) {
-  _stepTime = stepTime;
-  _initialTemperature = sensors.getTempCByIndex(0);
-  _endTemperature = temperature;
-  _gradient = (_endTemperature - _initialTemperature) / _stepTime;
-
-  _count = 0;
-  timer.setTimer(60000,updateDesiredTemperature,_stepTime);
-
-
-}
-
-void updateDesiredTemperature () {
-  if (_count < _stepTime) {
-  _count++;
-  _desiredTemperature = _desiredTemperature + _count * _gradient;
-  if (_count == _stepTime) {
-    activeSirene = true;
-  }
+float valueHisterese ()
+{
+  if (activeHisterese) {
+    return histerese;
+  } else {
+    return 0;
   }
 }
 
-void reset() {
-  _gradient = 0;
-  _stepTime = 0;
-  _initialTemperature = 0;
-  _endTemperature = 0;
-  _currentTemperature = 0;
-  _desiredTemperature = 0;
-  _count = 0;
-}
-
-void callback_temperature() {
-  // call sensors.requestTemperatures() to issue a global temperature
-  // request to all devices on the bus
-  Serial.print("Requesting temperatures...");
+float currentTemperature ()
+{
   sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-  // After we got the temperatures, we can print them here.
-  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
   Serial.print("Temperature for the device 1 (index 0) is: ");
   Serial.println(sensors.getTempCByIndex(0));
+  return sensors.getTempCByIndex(0);
 }
